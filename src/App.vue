a3d42e18b257964adcfccc05c840c076cbafe9bb
油炸丘比特 <8352067+fried-cupid@user.noreply.gitee.com>
2021-05-25 12:12:27 +0800

    第二次通过git命令修改

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
+
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <h2>哈哈哈哈我是git命令创键</h2>
     <router-view/>
   </div>
 </template>
